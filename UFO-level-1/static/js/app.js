afc69272e407f326f41ae47dcd1efc5db40216a3
leah-handel <leah.handel@gmail.com>
2021-03-25 13:09:58 -0500

    table filtering works on hard coded date

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,31 @@
+var allSightings = data;
+
+var tbody = d3.select("tbody");
+
+var button = d3.select("button");
+var dateField = d3.select("#date-input");
+
+function fillTable(sightings){
+    sightings.forEach(function(sighting) {
+        var row = tbody.append("tr");
+        Object.values(sighting).forEach(function(value){
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
+
+function handleClick(){
+    console.log("did get click");
+    tbody.html("");
+    var dateInput = moment(dateField.property("value"));
+    console.log(dateField.property("value"));
+    console.log(dateInput);
+    var filteredSightings = allSightings.filter(sighting => sighting.datetime == "1/5/2010");
+    fillTable(filteredSightings);
+}
+
+fillTable(allSightings);
+
+button.on("click", handleClick);
+
