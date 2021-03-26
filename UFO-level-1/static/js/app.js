b02a80171ebb9091ae95a87b456367abcbc03aa7
leah-handel <leah.handel@gmail.com>
2021-03-26 14:45:27 -0500

    text filters working

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,15 +1,23 @@
 var allSightings = data;
 
 var tbody = d3.select("tbody");
-
 var button = d3.select("button");
+
 var dateField = d3.select("#date-input");
 var cityField = d3.select("#city-input");
 var stateField = d3.select("#state-input");
 var countryField = d3.select("#country-input");
 var shapeField = d3.select("#shape-input");
 
+var textFields = [
+["city", cityField.property("value")],
+["state", stateField.property("value")],
+["country", countryField.property("value")],
+["shape", shapeField.property("value")]
+];
+
 function fillTable(sightings){
+    tbody.html("");
     sightings.forEach(function(sighting) {
         var row = tbody.append("tr");
         Object.values(sighting).forEach(function(value){
@@ -19,15 +27,51 @@ function fillTable(sightings){
     });
 };
 
+//function checkText(key, value, list){
+  //  list = list.filter(l =>l[key] === value);
+
+//};
+
 function handleClick(){
-    console.log("did get click");
-    tbody.html("");
+    d3.event.preventDefault();
+    console.log("did get click")
+
+    var dateField = d3.select("#date-input");
+
     var dateInput = moment(dateField.property("value"));
     console.log(dateField.property("value"));
     console.log(dateInput);
     var filteredSightings = allSightings.filter(sighting => moment(sighting.datetime, "M/D/YYYY").isSame(dateInput));
+
+    var cityField = d3.select("#city-input");
+    var stateField = d3.select("#state-input");
+    var countryField = d3.select("#country-input");
+    var shapeField = d3.select("#shape-input");
+    
+    var textFields = [
+    ["city", cityField.property("value")],
+    ["state", stateField.property("value")],
+    ["country", countryField.property("value")],
+    ["shape", shapeField.property("value")]
+    ];
+
+    //var cityField = d3.select("#city-input");
+    //var cityInput = cityField.property("value");
+    //field = "city"
+    //filteredSightings = filteredSightings.filter(sighting=>sighting[field]===cityInput);
+
+    //checkText("city", cityInput, filteredSightings);
+
+    textFields.forEach(function(array){
+        var inputType = array[0];
+        var inputText = array[1];
+        console.log(inputText);
+        if (!(inputText=="")){
+            filteredSightings = filteredSightings.filter(s =>s[inputType] == inputText);
+        };
+   });
     fillTable(filteredSightings);
-}
+};
 
 fillTable(allSightings);
 
